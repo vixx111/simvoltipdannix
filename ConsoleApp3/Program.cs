faca22b54d99d7a79cf531c1cf65a7e5891cd8c4
Admin <Admin@DESKTOP-UGP0C2G>
2024-11-20 09:56:37 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿char[] arr = { 'a', 'b', 'c', 'x', 'a', 'b', 'c', 'y', 'a', 'b', 'c', 'z' };
+string str = new string(arr);
+string searchPattern = "abc";
+int count = 0;
+int index = 0;
+while ((index = str.IndexOf(searchPattern, index)) != -1)
+{
+    count++;
+    index += searchPattern.Length;
+}
+Console.WriteLine($"Количество вхождений 'abc': {count}");
+ 
